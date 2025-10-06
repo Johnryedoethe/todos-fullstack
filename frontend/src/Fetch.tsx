993073ce4767019387acfa846c497c93d99bdd07
nezh <randomnameiscoolokay@gmail.com>
2025-10-06 22:31:12 +1100

    add input

--- frontend/src/Fetch.tsx
+++ frontend/src/Fetch.tsx
@@ -1,25 +1,32 @@
 import { useState } from "react"
 
 export function Fetch() {
+
+  const [targetName, setTargetName] = useState("")
+
   const [name, setName] = useState("")
   const [age, setAge] = useState(0)
+  const [count, setCount] = useState(0)
 
   async function fetchData() {
-    const res = await fetch("https://api.agify.io/?name=alice")
+    const res = await fetch(`https://api.agify.io/?name=${targetName}`)
     const data = await res.json()
     setName(data.name)
     setAge(data.age)
+    setCount(data.count)
   }
 
   return (
     <>
       <div>Fetch Component</div>
+      <input type="text" value={targetName} onChange={((e) => setTargetName(e.target.value))}></input>
       <button onClick={fetchData}>Fetch</button>
 
       {name !== "" && (
         <>
-          <p>age: {name}</p>
-          <p>name: {age}</p>
+          <p>name: {name}</p>
+          <p>age: {age}</p>
+          <p>person count: {count}</p>
         </>
       )}
     </>
