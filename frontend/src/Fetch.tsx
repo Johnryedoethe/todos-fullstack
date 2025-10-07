0a22f4c80a225892eb7a41ad8cb01bf467ececb7
Ngoc Dao <ngocdaothanh@gmail.com>
2025-10-08 09:22:51 +1100

    Improve Fetch

--- frontend/src/Fetch.tsx
+++ frontend/src/Fetch.tsx
@@ -1,33 +1,67 @@
 import { useState } from "react"
 
 export function Fetch() {
-
   const [targetName, setTargetName] = useState("")
+  const [errorMsg, setErrorMsg] = useState("")
 
   const [name, setName] = useState("")
   const [age, setAge] = useState(0)
   const [count, setCount] = useState(0)
 
   async function fetchData() {
-    const res = await fetch(`https://api.agify.io/?name=${targetName}`)
-    const data = await res.json()
-    setName(data.name)
-    setAge(data.age)
-    setCount(data.count)
+    const name = targetName.trim()
+    if (name === "") {
+      setErrorMsg("Please enter a name")
+      return
+    }
+
+    setErrorMsg("")
+
+    const res = await fetch(`https://api.agify.io/?name=${name}`)
+    if (res.ok) {
+      const data = await res.json()
+      if (data.age) {
+        setName(data.name)
+        setAge(data.age)
+        setCount(data.count)
+      } else {
+        setErrorMsg("User not found")
+      }
+    } else {
+      setErrorMsg("Error fetching data: " + await res.text())
+    }
+  }
+
+  const updateEditName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTargetName(e.target.value)
   }
 
   return (
     <>
-      <div>Fetch Component</div>
-      <input type="text" value={targetName} onChange={((e) => setTargetName(e.target.value))} placeholder="enter a name..."></input>
-      <button onClick={fetchData}>Fetch</button>
+      <h2>Fetch Component</h2>
+
+      <input
+        type="text"
+        value={targetName}
+        onChange={updateEditName}
+        placeholder="enter a name..."
+      />
+
+      <button
+        onClick={fetchData}
+        disabled={targetName === ""}
+      >
+        Fetch
+      </button>
+
+      {errorMsg !== "" && <p style={{ color: "red" }}>{errorMsg}</p>}
 
       {name !== "" && (
-        <>
-          <p>name: {name}</p>
-          <p>age: {age}</p>
-          <p>person count: {count}</p>
-        </>
+        <ul>
+          <li>name: {name}</li>
+          <li>age: {age}</li>
+          <li>person count: {count}</li>
+        </ul>
       )}
     </>
   )
